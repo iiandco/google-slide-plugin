13289a20ff1c4b381907666e74879c047eec2ba1
root <root@development.pupaproj.ru>
2023-03-30 22:31:45 +0300

    123

--- src/GoogleSlideAPI.ts
+++ src/GoogleSlideAPI.ts
@@ -1,27 +1,67 @@
-import { google, slides_v1, drive_v3 } from "googleapis";
-import { GoogleAuth, OAuth2Client } from "googleapis-common";
+import { google, slides_v1 } from "googleapis";
+import { OAuth2Client } from "google-auth-library";
+import { getAuthCode } from "./getAuthCode";
 
-export async function getGoogleSlideAPI(accessToken: string): Promise<slides_v1.Resource$Presentations> {
-  const auth = new OAuth2Client();
-  auth.setCredentials({ access_token: accessToken });
+export class GoogleSlidesApi {
+  private static instance: GoogleSlidesApi;
+  private slidesApi: slides_v1.Slides;
 
-  const slidesAPI = google.slides({ version: "v1", auth });
-  return slidesAPI.presentations;
-}
+  private constructor(authClient: OAuth2Client) {
+    this.slidesApi = google.slides({ version: "v1", auth: authClient });
+  }
 
-export async function getGoogleDriveAPI(accessToken: string): Promise<drive_v3.Resource$Files> {
-  const auth = new OAuth2Client();
-  auth.setCredentials({ access_token: accessToken });
+  public static async getInstance(): Promise<GoogleSlidesApi> {
+    if (!this.instance) {
+      const authClient = await this.authorize();
+      this.instance = new GoogleSlidesApi(authClient);
+    }
+    return this.instance;
+  }
 
-  const driveAPI = google.drive({ version: "v3", auth });
-  return driveAPI.files;
-}
+  private static async authorize(): Promise<OAuth2Client> {
+    const clientId = '802627914353-ssgj5hvgnjr66k7dg7a2ps8eluflbiu5.apps.googleusercontent.com';
+    const clientSecret = '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';
+    const redirectUri = 'https://googleplugin.pupaproj.ru';
+  
+    if (!clientId || !clientSecret || !redirectUri) {
+      throw new Error("Missing OAuth2 client configuration");
+    }
+  
+    const oauth2Client = new OAuth2Client(clientId, clientSecret, redirectUri);
+  
+    // Получите код авторизации, используя функцию getAuthCode
+    const authCode = await getAuthCode(oauth2Client);
+  
+    // Обменяйте код авторизации на токены доступа и обновления
+    const { tokens } = await oauth2Client.getToken(authCode);
+    oauth2Client.setCredentials(tokens);
+  
+    return oauth2Client;
+  }
+  
 
-export async function getPresentations(driveAPI: drive_v3.Resource$Files): Promise<drive_v3.Schema$File[]> {
-  const response = await driveAPI.list({
-    q: "mimeType='application/vnd.google-apps.presentation'",
-    fields: "nextPageToken, files(id, name)",
-  });
+  public async getSelectedElement(presentationId: string): Promise<{ text: string; type: string }> {
+    const response = await this.slidesApi.presentations.get({ presentationId });
+    const currentPageId = response.data.slides?.[0].objectId;
 
-  return response.data.files || [];
+    if (!currentPageId) {
+      throw new Error("No slides found");
+    }
+
+    const pageElements = response.data.slides[0].pageElements;
+
+    if (!pageElements) {
+      throw new Error("No elements found on the slide");
+    }
+
+    // Выберите первый элемент на слайде
+    const selectedElement = pageElements[0];
+    const elementType = selectedElement.elementType;
+    const elementText = selectedElement.shape?.text?.textElements?.[0].textRun?.content;
+
+    return {
+      text: elementText || "",
+      type: elementType || "",
+    };
+  }
 }
--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,71 +1,46 @@
-import React, { useState, useEffect } from "react";
-import { getGoogleSlideAPI } from "./GoogleSlideAPI";
+import React, { useEffect, useState } from "react";
+import { GoogleSlidesApi } from "./GoogleSlidesApi.ts";
 
-interface SidebarProps {
-  accessToken: string;
-  presentationId: string;
+interface SelectedElement {
+  text: string;
+  type: string;
 }
 
-export const Sidebar: React.FC<SidebarProps> = ({ accessToken, presentationId }) => {
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [elementIndex, setElementIndex] = useState(0);
-  const [selectedElement, setSelectedElement] = useState<{ text: string; type: string } | null>(null);
+const Sidebar: React.FC = () => {
+  const [selectedElement, setSelectedElement] = useState<SelectedElement | null>(null);
 
   useEffect(() => {
-    async function getSelectedElement() {
-      const slideAPI = await getGoogleSlideAPI(accessToken);
-      const presentation = await slideAPI.get({ presentationId });
-
-      if (presentation.data && presentation.data.slides) {
-        const slide = presentation.data.slides[slideIndex];
-        const element = slide.pageElements && slide.pageElements[elementIndex];
-
-        if (element && element.shape && element.shape.text && element.shape.text.textElements) {
-          setSelectedElement({
-            text: element.shape.text.textElements
-              .map((textElement) => textElement.textRun?.content)
-              .filter((content) => content)
-              .join(""),
-            type: element.shape.shapeType || "",
-          });
-        } else {
-          setSelectedElement(null);
-        }
+    const fetchSelectedElement = async (presentationId: string) => {
+      try {
+        const api = await GoogleSlidesApi.getInstance();
+        const element = await api.getSelectedElement(presentationId);
+        setSelectedElement(element);
+      } catch (error) {
+        console.error("Error fetching selected element:", error);
       }
-    }
+    };
 
-    getSelectedElement();
-  }, [accessToken, presentationId, slideIndex, elementIndex]);
+    // Замените 'your-presentation-id' на ID вашей презентации
+    fetchSelectedElement("your-presentation-id");
+  }, []);
 
   return (
     <div>
-      <label>
-        Индекс слайда:
-        <input
-          type="number"
-          value={slideIndex}
-          onChange={(e) => setSlideIndex(parseInt(e.target.value, 10))}
-        />
-      </label>
-      <br />
-      <label>
-        Индекс элемента:
-        <input
-          type="number"
-          value={elementIndex}
-          onChange={(e) => setElementIndex(parseInt(e.target.value, 10))}
-        />
-      </label>
-      <br />
-      {selectedElement ? (
-        <>
-          <h2>Выбранный элемент</h2>
-          <p>Тип: {selectedElement.type}</p>
-          <p>Текст: {selectedElement.text}</p>
-        </>
-      ) : (
-        <p>Нет выбранного элемента</p>
-      )}
+  {selectedElement ? (
+    <div>
+      <h3>Selected Element</h3>
+      <p>
+        <strong>Type:</strong> {selectedElement.type}
+      </p>
+      <p>
+        <strong>Text:</strong> {selectedElement.text}
+      </p>
     </div>
-  );
+  ) : (
+    <p>No element selected</p>
+  )}
+</div>
+);
 };
+
+export default Sidebar;
